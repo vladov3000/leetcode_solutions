23dbfd964120c1dadc45ba06c944db84f91afb43
vladov <vladov3000@gmail.com>
2021-04-15 11:57:14 -0700

    Update 1151-minimum-swaps-to-group-all-1s-together.rs

--- 1151-minimum-swaps-to-group-all-1s-together.rs
+++ 1151-minimum-swaps-to-group-all-1s-together.rs
@@ -1,5 +1,5 @@
 impl Solution {
-    pub fn min_swaps(data: Vec<i32>) -> i32 {
+    pub fn min_swaps1(data: Vec<i32>) -> i32 {
         let total: i32 = data.iter().sum();
         let mut prev = total - data.iter().take(total as usize).sum::<i32>();
         let mut res = prev;
@@ -12,5 +12,17 @@ impl Solution {
         }
         
         res
+    }
+    
+    pub fn min_swaps(data: Vec<i32>) -> i32 {
+        let total: i32 = data.iter().sum();
+        let init = total - data.iter().take(total as usize).sum::<i32>();
+        
+        data.iter().zip(data.iter().skip(total as usize)).fold((init, init), 
+            |(acc, prev), (start, new)| {
+                let cur = prev - (*start == 0) as i32 + (*new == 0) as i32;
+                (acc.min(cur), cur) 
+            }
+        ).0
     }
 }
