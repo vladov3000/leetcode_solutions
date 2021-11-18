c1b744702b9c4150f02e3513898cdff84875f30f
vladov <vladov3000@gmail.com>
2021-11-17 17:19:50 -0800

    Create 62-unique-paths.c

---
+++ 62-unique-paths.c
@@ -0,0 +1,13 @@
+static int dp[100];
+
+int uniquePaths(int m, int n){
+    for (int i = 0; i < n; i++) dp[i] = 1;
+    
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++) {
+            dp[j] += dp[j - 1];
+        }
+    }
+    
+    return dp[n - 1];
+}
